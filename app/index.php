68bd96cc967e4146300022079ae64ffaf53870af
DanieLazo17 <daniel.lazo92@gmail.com>
2021-06-24 20:37:46 -0300

    cambiamos ruteo

--- app/index.php
+++ app/index.php
@@ -50,7 +50,7 @@
     //
     $app->post('[/]', \UsuarioControlador::class . ":ValidarUsuario");
 
-    $app->group('[/]', function (RouteCollectorProxy $group) {
+    $app->group('/Registro', function (RouteCollectorProxy $group) {
         $group->post('/Nombre[/]', \UsuarioControlador::class . ':BuscarNombreDeUsuario' );
         $group->post('/UsuarioNuevo[/]', \UsuarioControlador::class . ':CrearUsuario' );
     });
