13a6e06267b5085b912805597dbe7d3ace1d1083
DanieLazo17 <daniel.lazo92@gmail.com>
2021-06-26 18:40:34 -0300

    prueba

--- app/index.php
+++ app/index.php
@@ -50,6 +50,8 @@
     //
     $app->post('[/]', \UsuarioControlador::class . ":ValidarUsuario");
 
+    $app->post('/Registro/Nombre[/]', \UsuarioControlador::class . ":BuscarNombreDeUsuario");
+
     $app->group('/Registro', function (RouteCollectorProxy $gruporeg) {
         $gruporeg->post('/Nombre[/]', \UsuarioControlador::class . ':BuscarNombreDeUsuario' );
         $gruporeg->post('/UsuarioNuevo[/]', \UsuarioControlador::class . ':CrearUsuario' );
