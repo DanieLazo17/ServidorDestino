3f0ef8a67dcd01c6c425672d6715b29e2b65ba16
DanieLazo17 <daniel.lazo92@gmail.com>
2021-06-26 22:52:31 -0300

    cambiamos ruteo

--- app/index.php
+++ app/index.php
@@ -2,10 +2,10 @@
 
     use Psr\Http\Message\ResponseInterface as Response;
     use Psr\Http\Message\ServerRequestInterface as Request;
-    use Psr\Http\Server\RequestHandlerInterface;
+    //use Psr\Http\Server\RequestHandlerInterface;
     use Slim\Factory\AppFactory;
-    use Slim\Routing\RouteCollectorProxy;
-    use Slim\Routing\RouteContext;
+    //use Slim\Routing\RouteCollectorProxy;
+    //use Slim\Routing\RouteContext;
 
     require __DIR__ . '/../vendor/autoload.php';
 
