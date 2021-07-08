3d1affaf8311357a98f771d32f2f6d3364b7dade
DanieLazo17 <daniel.lazo92@gmail.com>
2021-07-08 19:49:06 -0300

    agregamos variables de entorno

--- app/accesoADatos/AccesoDatos.php
+++ app/accesoADatos/AccesoDatos.php
@@ -7,7 +7,7 @@
 
         private function __construct(){
             try {
-                $this->objetoPDO = new PDO('mysql:host=remotemysql.com:3306;dbname='.getenv("Database").';charset=utf8', getenv("Usuario"), getenv("Pass"), array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                $this->objetoPDO = new PDO('mysql:host='.getenv("ServidorMySQL").';dbname='.getenv("Database").';charset=utf8', getenv("Usuario"), getenv("Pass"), array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
                 $this->objetoPDO->exec("SET CHARACTER SET utf8");
             } catch (PDOException $e) {
                 print "Error: " . $e->getMessage();
