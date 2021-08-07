376290a6cc298d496ae9c5b6d0f403b828ff9911
DanieLazo17 <daniel.lazo92@gmail.com>
2021-08-07 20:04:32 -0300

    prueba de modifidador de acceso

--- app/controlador/UsuarioControlador.php
+++ app/controlador/UsuarioControlador.php
@@ -25,15 +25,15 @@
             if( count($arregloUsuario) == 1 ){
                 
                 $usuarioDB = new Usuario();
-                $ini = "set";
+                $es = "set";
 
                 foreach($arregloUsuario as $objetoUsuario){
 
                     foreach ($objetoUsuario as $atr => $valueAtr) {
 
-                        $ini = $ini . ucfirst($atr);
-                        $usuarioDB->$ini($valueAtr);
-                        $ini = "set";
+                        $es = $es . ucfirst($atr);
+                        $usuarioDB->$es($valueAtr);
+                        $es = "set";
                     }
                 }
                 
