2edd5d945bbfca01b2925f5f78eca0011050ab0a
DanieLazo17 <daniel.lazo92@gmail.com>
2021-08-07 22:34:32 -0300

    prueba de modifidador de acceso

--- app/entidades/Usuario.php
+++ app/entidades/Usuario.php
@@ -2,8 +2,8 @@
 
     class Usuario{
         
-        private $nombre;
-        private $contrasena;
+        public $nombre;
+        public $contrasena;
 
         public function __construct(){
             
