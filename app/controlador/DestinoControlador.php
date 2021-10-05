7eec2e68304a20081323696f34e13cdc678b1896
DanieLazo17 <daniel.lazo92@gmail.com>
2021-10-05 11:16:36 -0300

    normalizamos datos en controlador DestinoControlador

--- app/controlador/DestinoControlador.php
+++ app/controlador/DestinoControlador.php
@@ -9,20 +9,20 @@
             $pais = $listaDeParametros['pais'];
             $provincia = $listaDeParametros['provincia'];
 
+            //Normalizar datos
+            $nombre = ucwords($nombre);
+            $tipoTurismo = ucwords($tipoTurismo);
+            $pais = ucwords($pais);
+            $provincia = ucwords($provincia);
+
             $destino = new Destino();
-            //$destino->nombre = $listaDeParametros['nombre'];
-            //$destino->tipoTurismo = $listaDeParametros['tipoTurismo'];
-            //$destino->pais = $listaDeParametros['pais'];
-            //$destino->provincia = $listaDeParametros['provincia'];
             $destino->setNombre($nombre);
             $destino->setTipoTurismo($tipoTurismo);
             $destino->setPais($pais);
             $destino->setProvincia($provincia);
-
             $destino->guardarDestino();
 
             $response->getBody()->write( json_encode($destino) );
-
             return $response;
         }
 
@@ -39,8 +39,8 @@
             $Destino = new Destino();
             $Destino->setIdDestino($idDestino);
             $arregloDeDestino =$Destino->obtenerDestino();
+
             $response->getBody()->write(json_encode($arregloDeDestino));
-   
             return $response->withHeader('Content-Type', 'application/json');
         }
 
@@ -57,5 +57,4 @@
             return $response->withHeader('Content-Type', 'application/json');
         }
     }
-
 ?>
